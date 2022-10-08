5875b28713984ec5917822e2d4b3a391ca720b1a
claudio-germano <wantuir.germano@gmail.com>
2022-10-08 17:52:05 -0300

    nav bar links + waitlist wallet checker

--- js/wallet.js
+++ js/wallet.js
@@ -2,16 +2,22 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
 const moonMoonz = new ethers.Contract(
-	ADDRESSES.MoonMoonz,
-	ABIS.MoonMoonz,
-	signer
+    ADDRESSES.MoonMoonz,
+    ABIS.MoonMoonz,
+    signer
 );
 
-// const moonMoonzRewarder = new ethers.Contract(
-// 	ADDRESSES.MoonMoonzRewarder,
-// 	ABIS.MoonMoonzRewarder,
-// 	signer
-// );
+const moonMoonzRewarder = new ethers.Contract(
+    ADDRESSES.MoonMoonzRewarder,
+    ABIS.MoonMoonzRewarder,
+    signer
+);
+
+const moonMoonzWater = new ethers.Contract(
+    ADDRESSES.MoonMoonzWater,
+    ABIS.MoonMoonzWater,
+    signer
+);
 
 /* -------------------------------------------------------------------------- */
 /*                              General Functions                             */
@@ -19,48 +25,65 @@ const moonMoonz = new ethers.Contract(
 
 // Connect wallet
 async function connect() {
-	await provider.send("eth_requestAccounts", []);
+    await provider.send("eth_requestAccounts", []);
 
-	//get Accs and APIs
-	const acc = await ethereum.request({
-		method: "eth_accounts",
-	});
+    //get Accs and APIs
+    const acc = await ethereum.request({
+        method: "eth_accounts",
+    });
 
-	const resWl = await (
-		await fetch("https://moonmoonz-api.up.railway.app/wl/" + acc[0])
-	).json();
+    const resWl = await (
+        await fetch("https://moonmoonz-api.up.railway.app/wl/" + acc[0])
+    ).json();
 
-	const resVIP = await (
-		await fetch("https://moonmoonz-api.up.railway.app/vip/" + acc[0])
-	).json();
+    const resVIP = await (
+        await fetch("https://moonmoonz-api.up.railway.app/vip/" + acc[0])
+    ).json();
 
-	//check if WL or VIP
-	if (acc.length) {
-		if (resWl.proof) {
-			console.log(resWl.proof);
-			alert("You are whitelisted!");
-		} else if (resVIP.proof) {
-			console.log(resVIP.proof);
-			alert("You are VIP!");
-		} else {
-			console.error(resWl.error);
-			console.error(resVIP.error);
-			alert("Not VIP or Whitelisted!");
-		}
-	} else {
-		console.log("Metamask is not connected");
-	}
+    const resWaitList = await (
+        await fetch("https://moonmoonz-api.up.railway.app/waitlist/" + acc[0])
+    ).json();
 
-	window.location.reload();
+    //check if WL or VIP
+    if (acc.length) {
+        if (resWl.proof) {
+            console.log(resWl.proof);
+            alert("You are whitelisted!");
+        } else if (resVIP.proof) {
+            console.log(resVIP.proof);
+            alert("You are VIP!");
+        } else if (resWaitList.proof) {
+            console.log(resWaitList.proof);
+            alert("You are WaitListed!");
+        } else {
+            console.error(resWl.error);
+            console.error(resVIP.error);
+            alert("Not VIP or Whitelisted!");
+        }
+    } else {
+        console.log("Metamask is not connected");
+    }
+
+    window.location.reload();
 }
 
 // Get address if connected else return null
 async function getAddress() {
-	try {
-		return await signer.getAddress();
-	} catch {
-		return null;
-	}
+    try {
+        return await signer.getAddress();
+    } catch {
+        return null;
+    }
+}
+
+function storeError(error) {
+    console.log(error);
+    Alpine.store("error", error);
+    Alpine.start();
+}
+
+function getError(error) {
+    return error.error.message.split("execution reverted: ")[1];
 }
 
 /* -------------------------------------------------------------------------- */
@@ -68,113 +91,217 @@ async function getAddress() {
 /* -------------------------------------------------------------------------- */
 
 async function fetchProof(addr, sale) {
-	const response = await (await fetch(`${API}/${sale}/${addr}`)).json();
-	if ("error" in response) return storeError("Not whitelisted");
-	return response.proof;
+    try {
+        const response = await (await fetch(`${API}/${sale}/${addr}`)).json();
+        if ("error" in response) throw "Not whitelisted";
+
+        return response.proof;
+    } catch (error) {
+        storeError(error.message);
+    }
 }
 
 // Claim 1 token during vip sale
-async function claimVIP() {
-	const addr = await getAddress();
-	if (!addr) storeError("Wallet not connected");
+async function claimVIP(addr) {
+    const proof = await fetchProof(addr, "vip");
+    if (!proof) throw "Not whitelisted";
 
-	const proof = await fetchProof(addr, "vip");
-	if (!proof) storeError("Not whitelisted");
-
-	await moonMoonz.claimVIP(proof);
+    try {
+        await moonMoonz.claimVIP(proof);
+    } catch (error) {
+        storeError(getError(error));
+    }
 }
 
 // Claim 1 token during wl sale
-async function claimWL() {
-	const addr = await getAddress();
-	if (!addr) storeError("Wallet not connected");
+async function claimWL(addr) {
+    const proof = await fetchProof(addr, "wl");
+    if (!proof) throw "Not whitelisted";
 
-	const proof = await fetchProof(addr, "wl");
-	if (!proof) storeError("Not whitelisted");
-
-	await moonMoonz.claimWL(proof, { value: PRICE });
+    try {
+        await moonMoonz.claimWL(proof, { value: PRICE });
+    } catch (error) {
+        storeError(getError(error));
+    }
 }
 
 // Claim 1 token during wl sale
-async function claimWaitlist() {
-	const addr = await getAddress();
-	if (!addr) storeError("Wallet not connected");
+async function claimWaitlist(addr) {
+    const proof = await fetchProof(addr, "waitlist");
+    if (!proof) throw "Not whitelisted";
 
-	const proof = await fetchProof(addr, "waitlist");
-	if (!proof) storeError("Not whitelisted");
-
-	await moonMoonz.claimWaitlist(proof, { value: PRICE });
+    try {
+        await moonMoonz.claimWaitlist(proof, { value: PRICE });
+    } catch (error) {
+        storeError(getError(error));
+    }
 }
 
 // Claim 1 token during public sale
 async function claim() {
-	const addr = await getAddress();
-	if (!addr) storeError("Wallet not connected");
-
-	await moonMoonz.claim({ value: PRICE });
+    try {
+        await moonMoonz.claim({ value: PRICE });
+    } catch (error) {
+        storeError(getError(error));
+    }
 }
 
 // Claim a token depending on the sale state
 async function mint() {
-	const saleState = await getSaleState();
+    const addr = await getAddress();
+    if (!addr) storeError("Wallet not connected");
 
-	if (saleState === 0) storeError("Sale hasn't started yet!");
-	else if (saleState === 1) claimVIP();
-	else if (saleState === 2) claimWL();
-	else if (saleState === 3) claimWaitlist();
-	else if (saleState === 4) claim();
-}
+    const saleState = await getSaleState();
 
-function storeError(error) {
-	Alpine.store("error", error);
-	Alpine.start();
+    if (saleState === 0) storeError("Sale hasn't started yet!");
+    else if (saleState === 1) claimVIP(addr);
+    else if (saleState === 2) claimWL(addr);
+    else if (saleState === 3) claimWaitlist(addr);
+    else if (saleState === 4) claim();
 }
 
 // Get totalSupply
 async function getTotalSupply() {
-	try {
-		return await moonMoonz.totalSupply();
-	} catch {
-		return 0;
-	}
+    try {
+        return (await moonMoonz.totalSupply()).toNumber();
+    } catch {
+        console.error(error);
+        return 0;
+    }
 }
 
 // Get sale state
 async function getSaleState() {
-	try {
-		return await moonMoonz.saleState();
-	} catch {
-		return 0;
-	}
+    try {
+        return (await moonMoonz.saleState()).toNumber();
+    } catch {
+        console.error(error);
+        return 0;
+    }
+}
+
+// Get token metadata
+async function getMetadata(id) {
+    try {
+        const metadata = await (
+            await fetch(await moonMoonz.tokenURI(id))
+        ).json();
+        return { name: metadata.name, image: metadata.image };
+    } catch (error) {
+        console.error(error);
+        return {};
+    }
+}
+
+// Get tokens of account
+async function tokensOf() {
+    try {
+        const addr = await getAddress();
+        if (!addr) storeError("Wallet not connected");
+
+        const balance = (await moonMoonz.balanceOf(addr)).toNumber();
+        const tokens = [];
+
+        for (let i = 0; i < balance; i++) {
+            const id = (
+                await moonMoonz.tokenOfOwnerByIndex(addr, 0)
+            ).toNumber();
+            const timezone = await moonMoonz.timezoneOf(id);
+            const metadata = await getMetadata(id);
+
+            tokens.push({
+                id,
+                timezone,
+                ...metadata,
+            });
+        }
+
+        return tokens;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+// Returns if it's night at certain timezone
+function isNight(timezone) {
+    const START = 1640995200;
+
+    const start =
+        timezone == 0 ? START : timezone == 1 ? START + 18000 : START - 18000;
+
+    const elapsedToday = (Math.floor(Date.now() / 1000) - start) % 86400;
+
+    return elapsedToday < 21600 || elapsedToday >= 64800;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                         MoonMoonzRewarder Functions                        */
 /* -------------------------------------------------------------------------- */
 
-// async function deposit(ids) {
-// 	if (!(await getAddress())) return;
-// 	await moonMoonzRewarder.deposit(ids);
-// }
+async function deposit(ids) {
+    if (ids.length === 0) return;
 
-// async function withdraw(ids) {
-// 	if (!(await getAddress())) return;
-// 	await moonMoonzRewarder.withdraw(ids);
-// }
+    try {
+        await moonMoonzRewarder.deposit(ids);
+    } catch (error) {
+        storeError(getError(error));
+    }
+}
 
-// async function claim() {
-// 	if (!(await getAddress())) return;
-// 	await moonMoonzRewarder.claim();
-// }
+async function withdraw(ids) {
+    if (ids.length === 0) return;
 
-// async function claim() {
-// 	const addr = await getAddress();
-// 	if (!addr) return;
-// 	return await moonMoonzRewarder.earned(addr);
-// }
+    try {
+        await moonMoonzRewarder.withdraw(ids);
+    } catch (error) {
+        storeError(getError(error));
+    }
+}
 
-// async function depositsOf() {
-// 	const addr = await getAddress();
-// 	if (!addr) return;
-// 	return await moonMoonzRewarder.depositsOf(addr);
-// }
+async function claimWater() {
+    try {
+        await moonMoonzRewarder.claim();
+    } catch (error) {
+        storeError(getError(error));
+    }
+}
+
+async function earned() {
+    try {
+        const addr = await getAddress();
+        if (!addr) throw "Wallet not connected";
+
+        await moonMoonzRewarder.deposit(ids);
+    } catch (error) {
+        storeError(getError(error));
+    }
+}
+
+// Get deposited tokens of account
+async function depositsOf() {
+    try {
+        const addr = await getAddress();
+        if (!addr) storeError("Wallet not connected");
+
+        const ids = await moonMoonzRewarder.depositsOf(addr);
+        const tokens = [];
+
+        for (const _id of ids) {
+            const id = _id.toNumber();
+            const timezone = await moonMoonz.timezoneOf(id);
+            const metadata = await getMetadata(id);
+
+            tokens.push({
+                id,
+                timezone,
+                ...metadata,
+            });
+        }
+
+        return tokens;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
